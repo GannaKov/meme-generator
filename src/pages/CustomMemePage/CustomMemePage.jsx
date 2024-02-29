71d763da54497cc73385dc33c03384c0aca9176b
GannaKov <zlatta2000@gmail.com>
2024-02-29 17:34:14 +0100

    file label

--- src/pages/CustomMemePage/CustomMemePage.jsx
+++ src/pages/CustomMemePage/CustomMemePage.jsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
 import styles from "./CustomMemePage.module.css";
 
+import ColorBox from "../../components/ColorBox/ColorBox";
+import Form from "../../components/Form/Form";
+import MemeWrp from "../../components/MemeWrp/MemeWrp";
+
 const CustomMemePage = () => {
   const [imgSrc, setImgSrc] = useState(null);
+  const [textTop, setTextTop] = useState("");
+  const [textBottom, setTextBottom] = useState("");
+  const [textColor, setTextColor] = useState("black");
+  // const [imgSrc, setImgSrc] = useState(null);
+  const colors = ["red", "green", "black", "white"];
 
   useEffect(() => {
     return () => {
@@ -15,27 +24,56 @@ const CustomMemePage = () => {
   function handleFiles(e) {
     console.log(window.URL.createObjectURL(e.target.files[0]));
     setImgSrc(window.URL.createObjectURL(e.target.files[0]));
-    // window.URL.createObjectURL(files[0]);
+  }
+
+  function handleColorText(e) {
+    if (e.target.id) {
+      setTextColor(e.target.id);
+      console.log(e.target.id);
+    }
   }
   return (
     <div className={styles.pageWrp}>
-      <div className={styles.fileUploadWrp}>
+      <div>
+        {/* className={styles.fileUploadWrp} */}
         <input
           type="file"
           id="fileElem"
           accept="image/*"
-          // style={{ display: "none" }}
-
+          style={{ display: "none" }}
           onChange={handleFiles}
         />
-        <a href="#" id="fileSelect">
+        {/* <a href="#" id="fileSelect">
           Select some files
-        </a>
+        </a> */}
+        <label htmlFor="fileElem" className={styles.fileUploadLabel}>
+          Select some files
+        </label>
       </div>
 
-      <div id="fileList">
-        {imgSrc ? <img src={imgSrc} alt="" /> : <p>No files selected!</p>}
-      </div>
+      {imgSrc ? (
+        <>
+          <ColorBox colors={colors} handleColorText={handleColorText} />
+          <Form text={textTop} setText={setTextTop} forLabel="Text Top"></Form>
+          <div className={styles.memeCard}>
+            <MemeWrp
+              src={imgSrc}
+              alt="My meme"
+              textColor={textColor}
+              textTop={textTop}
+              textBottom={textBottom}
+            />
+          </div>
+
+          <Form
+            text={textBottom}
+            setText={setTextBottom}
+            forLabel="Text Bottom"
+          ></Form>
+        </>
+      ) : (
+        <p>No files selected!</p>
+      )}
     </div>
   );
 };
