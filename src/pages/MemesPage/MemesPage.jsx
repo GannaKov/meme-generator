3fcdaf6808d850a3b9b8be7d57ddf9104032e3c8
GannaKov <zlatta2000@gmail.com>
2024-03-01 13:03:10 +0100

    spinner

--- src/pages/MemesPage/MemesPage.jsx
+++ src/pages/MemesPage/MemesPage.jsx
@@ -6,9 +6,11 @@ import styles from "./MemesPage.module.css";
 import ColorBox from "../../components/ColorBox/ColorBox";
 import Form from "../../components/Form/Form";
 import SingleMemeWrp from "../../components/SingleMemeWrp/SingleMemeWrp";
+import Spinner from "../../components/Spinner";
 
 const MemesPage = () => {
   const memeContainerRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [memes, setMemes] = useState([]);
   const [textTop, setTextTop] = useState("");
   const [textBottom, setTextBottom] = useState("");
@@ -17,9 +19,13 @@ const MemesPage = () => {
   const colors = ["red", "green", "black", "white"];
 
   useEffect(() => {
+    setIsLoading(true);
     getAllMemes()
       .then((res) => setMemes(res.data.memes))
-      .catch((error) => console.log(error.message));
+      .catch((error) => console.log(error.message))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   function handleNextBtnClick() {
@@ -63,79 +69,63 @@ const MemesPage = () => {
         console.error("oops, something went wrong!", error);
       });
   }
-  // let item = document.getElementById("whiteboard-frame");
-  // console.log(item);
-  // domtoimage
-  //   .toPng(item)
-  //   .then((dataUrl) => {
-  //     domtoimage
-  //       .toPng(item)
-  //       .then((dataUrl1) => {
-  //         this.savedImage = dataUrl1;
-  //         console.log(this.savedImage);
-  //       })
-  //       .catch((error) => {
-  //         console.error("oops, something went wrong!", error);
-  //       });
-  //   })
-  //   .catch((error) => {
-  //     console.error("oops, something went wrong!", error);
-  //   });
+
   return (
     <div className={styles.pageWrp}>
-      <ColorBox colors={colors} handleColorText={handleColorText} />
+      {isLoading && <Spinner />}
+      {!isLoading && (
+        <>
+          <ColorBox colors={colors} handleColorText={handleColorText} />
+          <Form text={textTop} setText={setTextTop} forLabel="Text Top"></Form>
+          {memes.length > 0 && (
+            <div className={styles.memeCard}>
+              <p className={styles.memeTitle}>{memes[number].name}</p>
 
-      <Form text={textTop} setText={setTextTop} forLabel="Text Top"></Form>
-
-      {memes.length > 0 && (
-        <div className={styles.memeCard}>
-          <p className={styles.memeTitle}>{memes[number].name}</p>
-
-          <div ref={memeContainerRef}>
-            <SingleMemeWrp
-              src={memes[number].url}
-              alt={memes[number].name}
-              textColor={textColor}
-              textTop={textTop}
-              textBottom={textBottom}
-            />
+              <div ref={memeContainerRef}>
+                <SingleMemeWrp
+                  src={memes[number].url}
+                  alt={memes[number].name}
+                  textColor={textColor}
+                  textTop={textTop}
+                  textBottom={textBottom}
+                />
+              </div>
+            </div>
+          )}
+          {memes.length > 0 && (
+            <button
+              type="button"
+              className={styles.downloadBtn}
+              onClick={handleDownloadClick}
+            >
+              Download
+            </button>
+          )}
+          <Form
+            text={textBottom}
+            setText={setTextBottom}
+            forLabel="Text Bottom"
+          ></Form>
+          <div className={styles.memeBtnWrp}>
+            <button
+              type="button"
+              onClick={handlePrevBtnClick}
+              className={styles.memeBtn}
+              disabled={number === 0}
+            >
+              Previous
+            </button>
+            <button
+              type="button"
+              onClick={handleNextBtnClick}
+              className={styles.memeBtn}
+              disabled={number === memes.length - 1}
+            >
+              Next
+            </button>
           </div>
-        </div>
+        </>
       )}
-      {memes.length > 0 && (
-        <button
-          type="button"
-          className={styles.downloadBtn}
-          onClick={handleDownloadClick}
-        >
-          Download
-        </button>
-      )}
-
-      <Form
-        text={textBottom}
-        setText={setTextBottom}
-        forLabel="Text Bottom"
-      ></Form>
-
-      <div className={styles.memeBtnWrp}>
-        <button
-          type="button"
-          onClick={handlePrevBtnClick}
-          className={styles.memeBtn}
-          disabled={number === 0}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          onClick={handleNextBtnClick}
-          className={styles.memeBtn}
-          disabled={number === memes.length - 1}
-        >
-          Next
-        </button>
-      </div>
     </div>
   );
 };
